5889489950fef1c3e37f97e1204c1e0b28e5727b
王佳 <wangjia_1919@163.com>
2017-12-29 12:57:07 +0800

    服务 初始化

---
+++ src/main/java/com/springcloudfiy/eurekaclient/controller/DcController.java
@@ -0,0 +1,25 @@
+package com.springcloudfiy.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wj42134
+ * @date 2017 12 29 12:51
+ */
+@RestController
+public class DcController {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+
+}
