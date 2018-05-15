c9444e1146017729a31cbf3d52f2ef3a5c6c1d9d
Jarvis <wangjia_1919@163.com>
2018-05-15 11:19:49 +0800

    升级cloud应用

--- src/main/java/com/springcloudify/cloudy/eurekaclient/EurekaClientApplication.java
+++ src/main/java/com/springcloudify/cloudy/eurekaclient/EurekaClientApplication.java
@@ -1,11 +1,10 @@
 package com.springcloudify.cloudy.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringCloudApplication
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
